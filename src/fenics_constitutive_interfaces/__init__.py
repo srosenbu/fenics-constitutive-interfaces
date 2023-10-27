462cd62f4b8d8ed62ad19619a43c4c1498259919
srosenbu <sjard-mathis.rosenbusch@bam.de>
2023-10-27 15:36:33 +0200

    Basic structure

---
+++ src/fenics_constitutive_interfaces/__init__.py
@@ -0,0 +1,13 @@
+"""
+Copyright (c) 2023 Sjard Rosenbusch. All rights reserved.
+
+fenics-constitutive-interfaces: A great package.
+"""
+
+
+from __future__ import annotations
+
+from ._version import version as __version__
+from .interfaces import *
+
+__all__ = ["__version__"]
